8d57753daaf090479fca8cc13c7ed325a02a75dc
percyvelle <gomanov.09@mail.ru>
2022-09-27 02:24:51 +0300

    ref

--- src/VultusDatabaseManager.cpp
+++ src/VultusDatabaseManager.cpp
@@ -14,7 +14,7 @@ void VultusDatabaseManager::connectToDatabase()
 {
     m_manager.setHostName("localhost");
     m_manager.setPort(5432);
-    m_manager.setDatabaseName("Vultus");
+    m_manager.setDatabaseName("vultus");
     m_manager.setUserName("percy");
     m_manager.setPassword("gkpf2ci");
 
@@ -30,21 +30,20 @@ QJsonArray VultusDatabaseManager::getUsers()
     QJsonObject profile_object;
     QJsonArray profile_array;
 
-    m_query->exec("SELECT * FROM table_profile");
+    m_query->exec("SELECT * FROM profile_Table");
     while(m_query->next()){
         profile_object["id"] = m_query->value("id").toJsonValue();
-        profile_object["first_name"] = m_query->value("first_name").toJsonValue();
-        profile_object["middle_name"] = m_query->value("middle_name").toJsonValue();
-        profile_object["last_name"] = m_query->value("last_name").toJsonValue();
+        profile_object["full_name"] = m_query->value("full_name").toJsonValue();
         profile_object["position"] = m_query->value("position").toJsonValue();
         profile_object["subdivision"] = m_query->value("subdivision").toJsonValue();
         profile_object["phone"] = m_query->value("phone").toJsonValue();
-        profile_object["workphone"] = m_query->value("workphone").toJsonValue();
-        profile_object["workplace"] = m_query->value("workplace").toJsonValue();
-        profile_object["birthday"] = m_query->value("birthday").toString();
+        profile_object["work_phone"] = m_query->value("work_phone").toJsonValue();
+        profile_object["work_place"] = m_query->value("work_place").toJsonValue();
+        profile_object["birthday"] = m_query->value("birthday").toJsonValue();
         profile_object["description"] = m_query->value("description").toJsonValue();
         profile_object["status_text"] = m_query->value("status_text").toJsonValue();
-        profile_object["status"] = m_query->value("status").toJsonValue();
+        profile_object["status_name"] = m_query->value("status_name").toJsonValue();
+        profile_object["super_busy"] = m_query->value("super_busy").toJsonValue();
 
         profile_array.append(profile_object);
     }
@@ -57,22 +56,21 @@ QJsonArray VultusDatabaseManager::getAuth(const QString _login, const QString _p
     QJsonObject profile_main_object;
     QJsonArray profile_main_array;
 
-    m_query->exec("SELECT * FROM table_profile INNER JOIN table_auth ON table_profile.id=table_auth.id_profile");
+    m_query->exec("SELECT * FROM profile_table INNER JOIN auth_profile_table ON profile_table.id=auth_profile_table.id_profile");
     while(m_query->next()){
         if(m_query->value("login") == _login && m_query->value("password") == _password){
             profile_main_object["id"] = m_query->value("id").toJsonValue();
-            profile_main_object["first_name"] = m_query->value("first_name").toJsonValue();
-            profile_main_object["middle_name"] = m_query->value("middle_name").toJsonValue();
-            profile_main_object["last_name"] = m_query->value("last_name").toJsonValue();
+            profile_main_object["full_name"] = m_query->value("full_name").toJsonValue();
             profile_main_object["position"] = m_query->value("position").toJsonValue();
             profile_main_object["subdivision"] = m_query->value("subdivision").toJsonValue();
             profile_main_object["phone"] = m_query->value("phone").toJsonValue();
-            profile_main_object["workphone"] = m_query->value("workphone").toJsonValue();
-            profile_main_object["workplace"] = m_query->value("workplace").toJsonValue();
+            profile_main_object["work_phone"] = m_query->value("work_phone").toJsonValue();
+            profile_main_object["work_place"] = m_query->value("work_place").toJsonValue();
             profile_main_object["birthday"] = m_query->value("birthday").toJsonValue();
             profile_main_object["description"] = m_query->value("description").toJsonValue();
             profile_main_object["status_text"] = m_query->value("status_text").toJsonValue();
-            profile_main_object["status"] = m_query->value("status").toJsonValue();
+            profile_main_object["status_name"] = m_query->value("status_name").toJsonValue();
+            profile_main_object["super_busy"] = m_query->value("super_busy").toJsonValue();
         }
     }
 
